b17cccd4ce1dfb0414261b4e32b07607a4f356ef
fang <mymfang2010@gmail.com>
2020-05-12 17:17:22 +0800

    multi food in a row

--- order.py
+++ order.py
@@ -4,7 +4,7 @@ DISH_NUM = 12
 dish_name = ['披萨', '香蕉', '汉堡','吐司','披萨', '香蕉', '汉堡','吐司','披萨', '香蕉', '汉堡','吐司']
 
 def confirm_order():
-    sg.popup('下单成功')
+    sg.popup('下单成功', font='Any 15')
     # 向数据库写入订单数据
     reset_all()
     
@@ -14,6 +14,7 @@ def reset_all():
     # 初始化菜单界面
     pass
 
+'''
 def one_food(i):
     layout = [
         sg.Image(f'./img/food{i+1}.png', size=(200,200)), 
@@ -21,15 +22,31 @@ def one_food(i):
         #sg.Button(image_filename='./img/other/plus.png')
     ],
     return layout
+'''
 
+def one_food_modified(i):
+    a = sg.Image(f'./img/food{i+1}.png', size=(200,200))
+    b = sg.Text(dish_name[i], font='Any 20')
+    return a, b
+
+
+# menu页面
 menu_layout= []
 menu_layout += [sg.Text('面馆', font='Any 30')],
-for i in range(1, 3):
-    menu_layout += one_food(i)
-menu_layout += [sg.Button('确认选择'), sg.Button('清除所有')],
+for i in range(1, 4):
+    c = []
+    #menu_layout += one_food(i)
+    for j in range(3 * i - 2, 3 * i + 1):
+        a, b = one_food_modified(j)
+        c.append(a)
+        c.append(b)
+    menu_layout += c,
+menu_layout += [sg.Button('确认选择', font='Any 15'), sg.Button('清除所有', font='Any 15')],
 
+# 订单确认页面
 confirm_layout= []
 
+# 2个页面整合
 layout = [[sg.TabGroup([[sg.Tab('菜单', menu_layout), sg.Tab('确认界面', confirm_layout)]])]]
 
 window = sg.Window('菜单', layout)
@@ -45,10 +62,3 @@ while True:
         reset_all()
 
 window.close()
-
-'''
-    [sg.Image(filename=img1, size=(200,200), enable_events=True), sg.Text('披萨')],
-    [sg.Image(filename=img2, size=(200,200)), sg.Text('香蕉')],
-    [sg.Image(filename=img3, size=(200,200)), sg.Text('汉堡')],
-    [sg.Image(filename=img4, size=(200,200)), sg.Text('吐司')],
-'''
